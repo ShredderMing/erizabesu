c2f47c70e998ca5db0a7bd853bcc7410ed618277
ShredderMing <eternallovext@gmail.com>
2017-10-17 09:24:53 +0800

    swipe support

--- src/index.js
+++ src/index.js
@@ -2,36 +2,117 @@ import { h, Component } from 'preact';
 import './style.css';
 
 export default class Erizabesu extends Component {
-  state = { boardIndex: 0 };
+  static defaultProps = { autoPlay: true, allowMouseSwipe: true };
+  state = { boardIndex: 0, transition: true };
 
   componentDidMount() {
-    this.interval = setInterval(this.next.bind(this), 2000);
+    this.autoPlay();
+    this.dataLength = this.props.data.length;
+
+    this.boards.addEventListener('mousedown', this.onMouseDown);
+    this.boards.addEventListener('touchstart', this.onSwipeStart);
+    this.boards.addEventListener('touchmove', this.onSwipeMove);
+    this.boards.addEventListener('touchend', this.onSwipeEnd);
   }
 
   componentWillUnmount() {
+    this.clearAutoPlay();
+
+    this.boards.removeEventListener('mousedown', this.onMouseDown);
+    this.boards.removeEventListener('touchstart', this.onSwipeStart);
+    this.boards.removeEventListener('touchmove', this.onSwipeMove);
+    this.boards.removeEventListener('touchend', this.onSwipeEnd);
+  }
+
+  autoPlay = () => {
+    if (this.props.autoPlay && !this.interval) {
+      this.interval = setInterval(this.nextBoard, 2000);
+    }
+  };
+
+  clearAutoPlay = () => {
     if (this.interval) {
       clearInterval(this.interval);
+      this.interval = null;
     }
-  }
+  };
 
-  next() {
+  nextBoard = () => {
     this.setState({
-      boardIndex: (this.state.boardIndex + 1) % this.props.data.length
+      boardIndex: (this.state.boardIndex + 1) % this.dataLength
     });
-  }
+  };
 
-  render({ data, className, ...props }, { boardIndex }) {
+  onSwipeStart = e => {
+    this.clearAutoPlay();
+    this.setState({ transition: false });
+    this.startX = getX(e);
+    this.startIndex = this.state.boardIndex;
+  };
+
+  onSwipeMove = e => {
+    const deltaX = getX(e) - this.startX;
+    this.setState({
+      boardIndex: this.startIndex - deltaX / this.boards.clientWidth
+    });
+  };
+
+  onSwipeEnd = e => {
+    this.autoPlay();
+    this.setState({ transition: true });
+    let boardIndex = this.state.boardIndex;
+    if (boardIndex < 0) {
+      boardIndex = 0;
+    } else if (boardIndex > this.dataLength - 1) {
+      boardIndex = this.dataLength - 1;
+    } else {
+      boardIndex = Math.round(this.state.boardIndex);
+    }
+    this.setState({ boardIndex });
+  };
+
+  onMouseDown = e => {
+    e.preventDefault();
+    if (this.props.allowMouseSwipe) {
+      this.mouseDown = true;
+      document.addEventListener('mouseup', this.onMouseUp);
+      document.addEventListener('mousemove', this.onMouseMove);
+      this.onSwipeStart(e);
+    }
+  };
+
+  onMouseMove = e => {
+    if (this.mouseDown) {
+      this.onSwipeMove(e);
+    }
+  };
+
+  onMouseUp = e => {
+    this.mouseDown = false;
+    document.removeEventListener('mouseup', this.onMouseUp);
+    document.removeEventListener('mousemove', this.onMouseMove);
+    this.onSwipeEnd(e);
+  };
+
+  render({ data, className, ...props }, { boardIndex, transition }) {
     return (
       <div
         {...props}
         class={['erizabesu', props.class, className].filter(Boolean).join(' ')}
       >
         <div
+          ref={c => (this.boards = c)}
           class="boards"
-          style={{
-            transform: `translateX(${-boardIndex * 100}%)`,
-            transition: 'transform 0.5s ease'
-          }}
+          style={
+            transition
+              ? {
+                  transform: `translateX(${-boardIndex * 100}%)`,
+                  transition: 'transform 0.5s ease'
+                }
+              : {
+                  transform: `translateX(${-boardIndex * 100}%)`
+                }
+          }
         >
           {data.map(board => (
             <a class="board" style={{ backgroundImage: `url(${board.url})` }} />
@@ -40,4 +121,11 @@ export default class Erizabesu extends Component {
       </div>
     );
   }
+}
+
+function getX(e) {
+  if ('touches' in e) {
+    return e.touches[0].pageX;
+  }
+  return e.screenX;
 }
